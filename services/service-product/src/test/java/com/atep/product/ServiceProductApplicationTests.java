5c895769d22c4f9d404e2a9ff0b683ba0686f365
ymswktpd <1263552817@qq.com>
2025-04-22 00:48:23 +0800

    项目基础结构

---
+++ services/service-product/src/test/java/com/atep/product/ServiceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atep.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
